a2261a8051637208331d0d3939388d570fdbe198
Jonathan Dai <nightghost5078@gmail.com>
2019-04-25 10:49:44 +0800

    Nginx container bash script done

--- experiment/elasticsearch/run.sh
+++ experiment/elasticsearch/run.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname "$0")
+FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 function start_nginx() {
-    if [[ ! "$(docker ps -q -f name=nginx)" ]]; then
-        # nginx container exists
-        if [[ "$(docker ps -aq -f status=exited -f name=nginx)" ]]; then
-            # status exited, restart it
-            docker start nginx
-        fi
-        # do nothing with running status
-    else
-        # nginx container does not exist, create it
-        docker run -d --name nginx \
-            -p 8080:80 \
-            -v ${BASEDIR}/nginx/log:/var/log/nginx \
-            -v ${BASEDIR}/nginx/html:/usr/share/nginx/html \
-            nginx:1.16.0-alpine
+    if [[ "$(docker ps -aq -f status=exited -f name=nginx)" ]]; then
+        # status exited, restart it
+        echo "Nginx container status exited, restarting"
+        docker start nginx
+        return 0
     fi
+
+    if [[ "$(docker ps -aq -f status=running -f name=nginx)" ]]; then
+        # running status, ok
+        echo "Nginx container status running, ok"
+        return 0
+    fi
+
+    # nginx container does not exist, create it
+    echo "Nginx container not found, creating"
+    docker run -d --name nginx \
+        -p 8080:80 \
+        -v ${FULLPATH}/nginx/log:/var/log/nginx \
+        -v ${FULLPATH}/nginx/html:/usr/share/nginx/html \
+        nginx:1.16.0-alpine
+    docker ps -a
+
+    return 0
 }
 
-ES_PATH_CONF=/path/to/my/config ./bin/elasticsearch
+#ES_PATH_CONF=/path/to/my/config ./bin/elasticsearch
+
+start_nginx
