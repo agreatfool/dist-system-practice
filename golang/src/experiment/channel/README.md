c5a4f8afc79b506df38f98f6709fc215d8d51ebe
Jonathan Dai <nightghost5078@gmail.com>
2019-05-08 16:25:14 +0800

    Update channel test

--- golang/src/experiment/channel/README.md
+++ golang/src/experiment/channel/README.md
@@ -1,9 +1,28 @@
+Golang Channel Test
+===================
 
-* testValueCap：
-    * 测试对象：值
-    * 测试通道：有容量上限
-    * 测试目的：测试`值`在进出channel之后的地址变化
-* testPointerCap：
-    * 测试对象：指针
-    * 测试通道：有容量上限
-    * 测试目的：测试`指针`在进出channel之后的地址变化
+## testValueCap
+* 测试对象：值
+* 测试通道：有容量上限
+* 测试目的：测试`值`在进出channel之后的地址变化
+
+运行：
+
+```bash
+$ MODE=testValueCap go run channel.go
+# testValueCap: init:              (chan int)(0xc0000a6000)
+# testValueCap: values before channel: 1 0xc0000b0000, 2 0xc0000b0008
+# testValueCap: <- opt done:       (chan int)(0xc0000a6000)
+# testValueCap: values after  channel: 1 0xc0000b0020, 2 0xc0000b0028
+```
+
+## testPointerCap
+* 测试对象：指针
+* 测试通道：有容量上限
+* 测试目的：测试`指针`在进出channel之后的地址变化
+
+运行：
+
+```bash
+
+```
--- golang/src/experiment/channel/channel.go
+++ golang/src/experiment/channel/channel.go
@@ -1,13 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type point struct {
+	x, y int
+}
 
 func testValueCap() {
-	cint := make(chan int, 5)
+	cint := make(chan int, 2)
+	fmt.Printf("testValueCap: init:\t\t %#v\n", cint)
 
-	fmt.Printf("%#v\n", cint)
+	int1 := 1
+	int2 := 2
+
+	fmt.Printf("testValueCap: values before channel: %d %p, %d %p\n",
+		int1, &int1, int2, &int2)
+
+	cint <- int1
+	cint <- int2
+
+	fmt.Printf("testValueCap: <- opt done:\t %#v\n", cint)
+
+	intO1 := <-cint
+	intO2 := <-cint
+
+	fmt.Printf("testValueCap: values after  channel: %d %p, %d %p\n",
+		intO1, &intO1, intO2, &intO2)
+}
+
+func testPointerCap() {
+
 }
 
 func main() {
-	testValueCap()
+	mode := os.Getenv("MODE")
+
+	switch mode {
+	case "testValueCap":
+		testValueCap()
+	case "testPointerCap":
+		testPointerCap()
+	default:
+		fmt.Println("No env MODE specified, do nothing")
+	}
 }
