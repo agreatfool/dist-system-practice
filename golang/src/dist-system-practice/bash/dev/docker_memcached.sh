75799400fbae9fbcdb591cc5a942d74cac15ae2c
Jonathan Dai <nightghost5078@gmail.com>
2019-05-13 04:20:14 +0800

    Memcached docker bash done

--- golang/src/dist-system-practice/bash/dev/docker_memcached.sh
+++ golang/src/dist-system-practice/bash/dev/docker_memcached.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-docker run --rm -d \
-    --name memcached \
-    -p 11211:11211 \
-    memcached:1.5.14-alpine \
-    memcached \
-    -l "0.0.0.0" \
-    -p 11211 \
-    -m 64
+function start() {
+    docker run --rm -d \
+        --name memcached \
+        -p 11211:11211 \
+        memcached:1.5.14-alpine \
+        memcached \
+        -l "0.0.0.0" \
+        -p 11211 \
+        -m 64
+}
+
+function stop() {
+    docker stop memcached
+}
+
+function usage() {
+    echo "Usage: docker_memcached.sh start|stop"
+}
+
+if [[ $1 != "start" ]] && [[ $1 != "stop" ]]; then
+    usage
+    exit 0
+fi
+
+eval $1
