68b58329ca8318748f4cd3469725a75a2dbf8ef3
Jonathan Dai <nightghost5078@gmail.com>
2019-05-14 09:09:47 +0800

    Add model gorm property

--- golang/src/dist-system-practice/lib/model/work.go
+++ golang/src/dist-system-practice/lib/model/work.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Work struct {
-	Id          uint32    `json:"id" yaml:"id" gorm:"AUTO_INCREMENT"`
+	Id          uint32    `json:"id" yaml:"id" gorm:"AUTO_INCREMENT;primary_key"`
 	Viewed      uint32    `json:"viewed" yaml:"viewed" gorm:"default:0"`
-	Achievement string    `json:"achievement" yaml:"achievement" gorm:"default:''"`
+	Achievement string    `json:"achievement" yaml:"achievement" gorm:"default:'';size:256"`
 	IsPlanned   bool      `json:"isPlanned" yaml:"isPlanned" gorm:"default:false"`
 	Created     time.Time `json:"created" yaml:"created" gorm:"default:CURRENT_TIMESTAMP"`
 	Updated     time.Time `json:"updated" yaml:"updated" gorm:"default:CURRENT_TIMESTAMP"`
