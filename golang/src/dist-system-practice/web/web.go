b67138dd0737142621376d8fc5038c9ac567b0a6
Jonathan Dai <nightghost5078@gmail.com>
2019-05-14 14:55:33 +0800

    Web entrance done

--- golang/src/dist-system-practice/web/web.go
+++ golang/src/dist-system-practice/web/web.go
@@ -1,36 +1,45 @@
 package main
 
 import (
+	"dist-system-practice/lib/cache"
+	"dist-system-practice/lib/common"
+	"dist-system-practice/lib/dao"
+	"dist-system-practice/lib/database"
+	"dist-system-practice/lib/logger"
 	"dist-system-practice/web/handler"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
-// WEB入口 HTTP -> DefaultHandle 随机ID 随机业务 ->
-// Service gRPC -> 逻辑处理 -> 发送消息到Kafka
-// Consumer Kafka consumer -> 从Kafka拉取消息 -> 业务处理
-
-func handleEntrance(c *gin.Context) {
-	// 制作随机ID
-	// 选择随机业务
-
-	// 业务：
-	// 查询数据：设置一个百分比为404的情况，故意出错
-	// 更新简单字段：数字类型自增
-	// 查询计算值：该字段大部分情况下应该为默认值XXX，可以被查到，少部分为空，为空则触发更新操作
-	// 更新计算值：向消息队列里放入任务，等待处理
-
-	handler.HandlePlanTask(c)
-}
-
 func main() {
+	// initialize utilities
+	cache.New()
+	logger.New()
+	db := database.New()
+	dao.New()
+
+	defer db.Close()
+
+	// ensure env
+	if common.GetEnv("APP_NAME", "") == "" {
+		if err := os.Setenv("APP_NAME", "WEB"); err != nil {
+			panic(fmt.Sprintf("[WEB] Failed to set env APP_NAME: %s", err.Error()))
+		}
+	}
+
+	// web app
 	router := gin.Default()
 
-	router.GET("/plan", handleEntrance)
+	router.GET("/", handler.HandleIndex)
+	router.GET("/api", handler.HandleApi)
+
+	host := common.GetEnv("WEB_HOST", "0.0.0.0")
+	port := common.GetEnv("WEB_PORT", "8000")
 
 	s := &http.Server{
-		Addr:    ":8000",
+		Addr:    fmt.Sprintf("%s:%s", host, port),
 		Handler: router,
 	}
 
