6f8cadf3ebe3764bfed2586e0512f55a7279cf57
Jonathan Dai <nightghost5078@gmail.com>
2019-05-14 14:30:10 +0800

    Add memcached network

--- golang/src/dist-system-practice/bash/dev/docker_memcached.sh
+++ golang/src/dist-system-practice/bash/dev/docker_memcached.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 function start() {
+    docker network create memcached-net
+
     docker run --rm -d \
         --name memcached \
+        --network=memcached-net \
         -p 11211:11211 \
         memcached:1.5.14-alpine \
         memcached \
@@ -16,10 +19,14 @@ function stop() {
 }
 
 function usage() {
-    echo "Usage: docker_memcached.sh start|stop"
+    echo "Usage: docker_memcached.sh start|stop|clear"
 }
 
-if [[ $1 != "start" ]] && [[ $1 != "stop" ]]; then
+function clear() {
+    docker network rm memcached-net
+}
+
+if [[ $1 != "start" ]] && [[ $1 != "stop" ]] && [[ $1 != "clear" ]]; then
     usage
     exit 0
 fi
