a7848b8cd5f31b08b18af02aca6924a32c5128d9
Jonathan Dai <nightghost5078@gmail.com>
2019-05-15 14:30:50 +0800

    Save

--- golang/src/dist-system-practice/README.md
+++ golang/src/dist-system-practice/README.md
@@ -1,3 +1,5 @@
+dist-system-practice
+====================
 
 ## ENV
 * APP_NAME：应用名称：web、service、consumer，etc...
@@ -7,6 +9,7 @@
 
 * WEB_HOST：WEB应用的监听HOST
 * WEB_PORT：WEB应用的监听PORT
+* MAX_WORK_ID：最大可用WorkId，该数值是数据库中单表预建数据的数量值
 
 ## Versions
 ```bash
@@ -16,3 +19,13 @@ $ docker --version
 $ docker-compose --version
 # docker-compose version 1.23.2, build 1110ad01
 ```
+
+## TODO
+* zap的错误输出日志为空；分离业务和错误日志
+* zap的配置，以及类似caller之类的Encoder的使用
+* gin配置zap之后，有一条输出的日志里有latency，该时间的单位需要查证
+
+## Note
+* [Got "ambiguous import: found github.com/ugorji/go/codec in multiple modules" after upgraded to latest (1.3.1) #1673](https://github.com/gin-gonic/gin/issues/1673#issuecomment-482023570)
+* [cmd/go: <go module bug with github.com/ugorji/go > #29332](https://github.com/golang/go/issues/29332#issuecomment-448669442)
+* [怎么样输出错误日志到别外的一个文件中 #643](https://github.com/uber-go/zap/issues/643)
--- golang/src/dist-system-practice/web/handler/handler.go
+++ golang/src/dist-system-practice/web/handler/handler.go
@@ -1,13 +1,142 @@
 package handler
 
 import (
+	"dist-system-practice/lib/common"
+	"dist-system-practice/lib/logger"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/http"
 )
 
-func HandlePlanTask(c *gin.Context) {
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// Global
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+var apiMap = map[int]string{
+	10: "apiSearchWork",
+}
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// Structure
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+type result struct {
+	Code int
+	Data gin.H
+}
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// Handlers
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+// WEB入口 HTTP -> DefaultHandle 随机ID 随机业务 ->
+// Service gRPC -> 逻辑处理 -> 发送消息到Kafka
+// Consumer Kafka consumer -> 从Kafka拉取消息 -> 业务处理
+
+func HandleIndex(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"message": "Done",
+		"message": "Index",
 	})
+}
+
+func HandleApi(c *gin.Context) {
+	var res result
+	var err error
+
+	apiGenerated := randApi()
+	workId := randWorkId()
+
+	switch apiGenerated {
+	case "apiSearchWork":
+		recorder := common.NewTimeRecorder()
+		res, err = apiSearchWork(workId)
+		recorder.End()
+		logger.Get().Info("[WEB.Handler] Api: apiSearchWork.",
+			zap.String("api", "apiSearchWork"),
+			zap.Uint32("workId", workId),
+			zap.Float64("consumed", recorder.Elapsed()))
+	case "apiUpdateViewed":
+		recorder := common.NewTimeRecorder()
+		res, err = apiUpdateViewed(workId)
+		recorder.End()
+		logger.Get().Info("[WEB.Handler] Api: apiUpdateViewed.",
+			zap.String("api", "apiUpdateViewed"),
+			zap.Uint32("workId", workId),
+			zap.Float64("consumed", recorder.Elapsed()))
+	case "apiGetAchievement":
+		recorder := common.NewTimeRecorder()
+		res, err = apiGetAchievement(workId)
+		recorder.End()
+		logger.Get().Info("[WEB.Handler] Api: apiGetAchievement.",
+			zap.String("api", "apiGetAchievement"),
+			zap.Uint32("workId", workId),
+			zap.Float64("consumed", recorder.Elapsed()))
+	case "apiPlanWork":
+		recorder := common.NewTimeRecorder()
+		res, err = apiPlanWork(workId)
+		recorder.End()
+		logger.Get().Info("[WEB.Handler] Api: apiPlanWork.",
+			zap.String("api", "apiPlanWork"),
+			zap.Uint32("workId", workId),
+			zap.Float64("consumed", recorder.Elapsed()))
+	default:
+		err := errors.New(fmt.Sprintf("invalid api generated: %s", apiGenerated))
+		logger.Get().Error("[WEB.Handler] HandleApi: Invalid api generated.", zap.Error(err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err != nil {
+		logger.Get().Error("[WEB.Handler] HandleApi: Error.", zap.Error(err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	} else {
+		c.JSON(res.Code, res.Data)
+	}
+}
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// Apis
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+func apiSearchWork(id uint32) (result, error) {
+	var res result
+	return res, nil
+}
+
+func apiUpdateViewed(id uint32) (result, error) {
+	var res result
+	return res, nil
+}
+
+func apiGetAchievement(id uint32) (result, error) {
+	var res result
+	return res, nil
+}
+
+func apiPlanWork(id uint32) (result, error) {
+	var res result
+	return res, nil
+}
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// Tools
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+func randApi() string {
+	return "apiSearchWork"
+}
+
+func randWorkId() uint32 {
+	maxWorkId, err := common.StrToInt(common.GetEnv("MAX_WORK_ID", "10000000"))
+	if err != nil {
+		return 10000000 // return default value when error encountered
+	}
+
+	return uint32(common.RandomInt(1, maxWorkId))
 }
