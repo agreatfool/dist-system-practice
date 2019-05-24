3007058bf78129a6b00f3302dd85c29d5a95df37
Jonathan Dai <nightghost5078@gmail.com>
2019-05-24 16:32:04 +0800

    Save elk cluster config related

--- golang/src/dist-system-practice/bash/dev/docker_elasticsearch.sh
+++ golang/src/dist-system-practice/bash/dev/docker_elasticsearch.sh
@@ -9,20 +9,47 @@ BASEPATH="${FULLPATH}/../.."
 # logs/gc.log
 
 function start() {
-    echo "start"
+    cp ${BASEPATH}/conf/elasticsearch.yaml /private/tmp/elasticsearch.yaml
+    cp ${BASEPATH}/conf/filebeat.yaml /private/tmp/filebeat.yaml
+
+    docker-compose -f ${BASEPATH}/conf/elk-cluster.yaml -p "elk" up -d
 }
 
 function stop() {
-    echo "stop"
+    docker-compose -f ${BASEPATH}/conf/elk-cluster.yaml -p "elk" down -v # also remove containers & volumes
+}
+
+function logs() {
+    docker logs $1
+}
+
+function health() {
+    curl -X GET "http://127.0.0.1:9201/_cluster/health?pretty"
+}
+
+function cluster() {
+    curl -X GET "http://127.0.0.1:9201/_cluster/state?pretty"
+}
+
+function cluster_human() {
+    curl -X GET "http://127.0.0.1:9201/_cluster/stats?human&pretty"
+}
+
+function node() {
+    curl -X GET "http://127.0.0.1:9201/_nodes/$1/stats?pretty"
+}
+
+function index() {
+    curl -X GET "http://127.0.0.1:9201/_nodes/stats/indices?pretty"
 }
 
 function usage() {
-    echo "Usage: docker_elasticsearch.sh start|stop"
+    echo "Usage: docker_elasticsearch.sh start|stop|logs|health|cluster|cluster_human|node|index"
 }
 
-if [[ $1 != "start" ]] && [[ $1 != "stop" ]]; then
+if [[ $1 != "start" ]] && [[ $1 != "stop" ]] && [[ $1 != "logs" ]] && [[ $1 != "health" ]] && [[ $1 != "cluster" ]] && [[ $1 != "cluster_human" ]] && [[ $1 != "node" ]] && [[ $1 != "index" ]]; then
     usage
     exit 0
 fi
 
-eval $1
+eval $1 $2
