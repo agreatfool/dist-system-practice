92200a075d6be7a24b0da0206e6e2847908b18b9
Jonathan Dai <nightghost5078@gmail.com>
2019-05-28 21:41:50 +0800

    Enhance log script

--- golang/src/dist-system-practice/bash/dev/log.sh
+++ golang/src/dist-system-practice/bash/dev/log.sh
@@ -3,23 +3,36 @@
 FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BASEPATH="${FULLPATH}/../.."
 
+LOG_WEB="/tmp/logs/app/app.web.stdout.log"
+LOG_SERVICE="/tmp/logs/app/app.service.stdout.log"
+LOG_CONSUMER="/tmp/logs/app/app.consumer.stdout.log"
+
 function web() {
-    tail -f "/tmp/logs/app/app.web.stdout.log"
+    tail -f ${LOG_WEB}
 }
 
 function service() {
-    tail -f "/tmp/logs/app/app.service.stdout.log"
+    tail -f ${LOG_SERVICE}
 }
 
 function consumer() {
-    tail -f "/tmp/logs/app/app.consumer.stdout.log"
+    tail -f ${LOG_CONSUMER}
+}
+
+function reset() {
+    rm ${LOG_WEB}
+    touch ${LOG_WEB}
+    rm ${LOG_SERVICE}
+    touch ${LOG_SERVICE}
+    rm ${LOG_CONSUMER}
+    touch ${LOG_CONSUMER}
 }
 
 function usage() {
-    echo "Usage: log.sh web|service|consumer"
+    echo "Usage: log.sh web|service|consumer|reset"
 }
 
-if [[ $1 != "web" ]] && [[ $1 != "service" ]] && [[ $1 != "consumer" ]]; then
+if [[ $1 != "web" ]] && [[ $1 != "service" ]] && [[ $1 != "consumer" ]] && [[ $1 != "reset" ]]; then
     usage
     exit 0
 fi
