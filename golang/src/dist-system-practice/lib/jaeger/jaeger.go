6353fc9d61bbf5c8718cfe8b6e8fada8aca62f86
Jonathan Dai <nightghost5078@gmail.com>
2019-05-31 22:29:05 +0800

    Enhance jaeger tracer instance

--- golang/src/dist-system-practice/lib/jaeger/jaeger.go
+++ golang/src/dist-system-practice/lib/jaeger/jaeger.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/opentracing/opentracing-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
+	"github.com/uber/jaeger-lib/metrics"
 )
 
 func Get() opentracing.Tracer {
@@ -16,7 +18,13 @@ func New() opentracing.Tracer {
 		panic(fmt.Sprintf("[Jaeger] Could not parse Jaeger env vars: %s", err.Error()))
 	}
 
-	tracer, _, err := cfg.NewTracer()
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	tracer, _, err := cfg.NewTracer(
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
 	if err != nil {
 		panic(fmt.Sprintf("[Jaeger] Could not initialize jaeger tracer: %s", err.Error()))
 	}
