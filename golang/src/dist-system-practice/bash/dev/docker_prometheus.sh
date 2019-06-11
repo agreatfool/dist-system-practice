09bfe834f11736cd79c35ff9e41f08fb19b962e4
Jonathan Dai <nightghost5078@gmail.com>
2019-06-11 15:39:25 +0800

    Enhance prometheus cluster config

--- golang/src/dist-system-practice/bash/dev/docker_prometheus.sh
+++ golang/src/dist-system-practice/bash/dev/docker_prometheus.sh
@@ -3,6 +3,25 @@
 FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BASEPATH="${FULLPATH}/../.."
 
+# prometheus
+# /bin/prometheus
+# /prometheus/data
+# /etc/prometheus
+# console_libraries  consoles  prometheus.yml
+# /usr/share/prometheus
+# console_libraries  consoles
+
+# grafana
+# /etc/grafana
+# grafana.ini  ldap.toml  provisioning
+# /etc/grafana/provisioning
+# dashboards  datasources  notifiers
+# /var/lib/grafana
+# grafana.db  plugins  png
+# /var/log/grafana
+# /usr/share/grafana
+# LICENSE  NOTICE.md  README.md  VERSION  bin  conf  public  scripts  tools
+
 function start() {
     rm -rf /private/tmp/prom-master.yaml
     cp ${BASEPATH}/prometheus/prom-master.yaml /private/tmp/prom-master.yaml
@@ -10,12 +29,18 @@ function start() {
     cp ${BASEPATH}/prometheus/machine_xx1_node.yaml /private/tmp/machine_xx1_node.yaml
     rm -rf /private/tmp/prometheus/machine_xx2_node.yaml
     cp ${BASEPATH}/prometheus/machine_xx2_node.yaml /private/tmp/machine_xx2_node.yaml
+    rm -rf /private/tmp/grafana.ini
+    cp ${BASEPATH}/prometheus/grafana.ini /private/tmp/grafana.ini
+
+    node
 
     docker-compose -f ${BASEPATH}/conf/dev/prometheus-cluster.yaml -p "dist" up -d
 }
 
 function stop() {
     docker-compose -f ${BASEPATH}/conf/dev/prometheus-cluster.yaml -p "dist" down -v # also remove containers & volumes
+
+    kill -9 $(pgrep -lfa "node_exporter" | awk '{print $1}')
 }
 
 function logs() {
@@ -26,6 +51,8 @@ function node() {
     mkdir -p /private/tmp/logs
     rm -rf /private/tmp/logs/node_exporter.log
     touch /private/tmp/logs/node_exporter.log
+
+    kill -9 $(pgrep -lfa "node_exporter" | awk '{print $1}')
 
     nohup ${BASEPATH}/prometheus/node_exporter \
         --no-collector.boottime \
