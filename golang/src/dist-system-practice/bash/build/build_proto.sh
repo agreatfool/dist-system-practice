569b7d11baafcfcbad46a447f9fa647a740b19c9
Jonathan Dai <nightghost5078@gmail.com>
2019-06-16 17:52:05 +0800

    Move proto building out of dev dir

--- golang/src/dist-system-practice/bash/build/build_proto.sh
+++ golang/src/dist-system-practice/bash/build/build_proto.sh
@@ -2,3 +2,8 @@
 
 FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BASEPATH="${FULLPATH}/../.."
+
+protoc \
+    --go_out=plugins=grpc:${BASEPATH}/message \
+    -I ${BASEPATH}/proto \
+    ${BASEPATH}/proto/*.proto
