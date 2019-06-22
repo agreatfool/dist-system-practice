82da1e923d28a040413226b23d2fc7e8a51764dd
Jonathan Dai <nightghost5078@gmail.com>
2019-06-22 14:16:02 +0800

    Prod cluster yaml first version done

--- golang/src/dist-system-practice/bash/prod/cluster.sh
+++ golang/src/dist-system-practice/bash/prod/cluster.sh
@@ -3,7 +3,23 @@
 FULLPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BASEPATH="${FULLPATH}/../.."
 
+# MYSQL
 MYSQL_USER="root"
 MYSQL_PWD="abc123_"
 MYSQL_DB="dist"
 MEMCACHED_MEM="2048" # MB
+
+# KAFKA
+KAFKA_JVM_MEM="6G"
+
+# CASSANDRA
+CAS_JVM_MEM="6G"
+CAS_HEAP_SIZE="4G"
+CAS_HEAP_NEWSIZE="800M" # see https://docs.datastax.com/en/archived/cassandra/2.1/cassandra/operations/ops_tune_jvm_c.html
+
+# ELASTICSEARCH
+ES_JVM_MEM="6G"
+
+# PROMETHEUS
+GRAFANA_USER="admin"
+GRAFANA_PWD="abc123_"
